8f8c1bcf1cc1db7fcf12c1cacda25ef98d1bb780
xizana <xizoooo102@gmail.com>
2025-02-06 14:08:12 +0400

    Initial Commit

---
+++ Peach And Goma 3D (SwiftUI)/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Peach And Goma 3D (SwiftUI)
+//
+//  Created by Lasha Khizanishvili on 06.02.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Peach And Goma 3D (SwiftUI)/Peach_And_Goma_3D__SwiftUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  Peach_And_Goma_3D__SwiftUI_App.swift
+//  Peach And Goma 3D (SwiftUI)
+//
+//  Created by Lasha Khizanishvili on 06.02.25.
+//
+
+import SwiftUI
+
+@main
+struct Peach_And_Goma_3D__SwiftUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
