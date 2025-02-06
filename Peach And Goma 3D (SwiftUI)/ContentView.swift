d4351a094559f99b7a22dc19a19c0880d27a23a9
xizana <xizoooo102@gmail.com>
2025-02-06 14:49:27 +0400

    add peach and goma scene.

--- Peach And Goma 3D (SwiftUI)/ContentView.swift
+++ Peach And Goma 3D (SwiftUI)/ContentView.swift
@@ -6,18 +6,33 @@
 //
 
 import SwiftUI
+import SplineRuntime
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            PeachandgomaContentView()
+                .ignoresSafeArea()
         }
-        .padding()
     }
 }
+
+
+
+
+struct PeachandgomaContentView: View {
+    var body: some View {
+        
+        // fetching from cloud
+        let url = URL(string: "https://build.spline.design/H-lklKX6san-viny1bEm/scene.splineswift")!
+
+        // fetching from local
+        // let url = Bundle.main.url(forResource: "scene", withExtension: "splineswift")!
+
+        SplineView(sceneFileURL: url).ignoresSafeArea(.all)
+    }
+}
+
 
 #Preview {
     ContentView()
